67e1bea8847e7bc54543d1050b469144650ccb9d
Artem Demo <artemdemo@gmail.com>
2019-05-04 09:31:18 +0300

    removed unused services

--- react-app/source/views/components/AppView.jsx
+++ react-app/source/views/components/AppView.jsx
@@ -5,12 +5,6 @@ import Container from '../../components/Container/Container';
 import Button from '../../components/Button/Button';
 import MainMenu from '../../components/MainMenu/MainMenu';
 
-// This is only test for dynamic import.
-// Just to be sure that it is not broken with new `webpack` version
-// or bacause of any other reason.
-import('../../services/asyncService')
-    .then(s => s.default());
-
 const AppView = (props) => {
     return (
         <Container>
