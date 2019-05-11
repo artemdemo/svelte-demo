adff20a0696ea5e233cb1c90c739ef0e17a657ae
Artem Demo <artemdemo@gmail.com>
2019-05-11 09:17:31 +0300

    [test-ui] fixed indentation

--- testUI/webpack.config.js
+++ testUI/webpack.config.js
@@ -2,25 +2,25 @@ const path = require('path');
 const nodeExternals = require('webpack-node-externals');
 
 module.exports = {
-  entry: path.resolve(__dirname, './src/lib/index.js'),
-  output: {
-    path: path.resolve(__dirname, './dist/lib'),
-    filename: 'index.js',
-    library: '',
-    libraryTarget: 'commonjs'
-  },
-  externals: [nodeExternals()],
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /(node_modules|bower_components)/,
-        loader: 'babel-loader'
-      },
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader']
-      }
-    ]
-  }
+    entry: path.resolve(__dirname, './src/lib/index.js'),
+    output: {
+        path: path.resolve(__dirname, './dist/lib'),
+        filename: 'index.js',
+        library: '',
+        libraryTarget: 'commonjs'
+    },
+    externals: [nodeExternals()],
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules|bower_components)/,
+                loader: 'babel-loader'
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            }
+        ]
+    }
 };
